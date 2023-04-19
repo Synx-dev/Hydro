f466828e463371b3728aa94a509f82280886e5e9
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2023-04-19 17:05:54 +0700

    Create iconlib.lua
    Main Icon For Library

---
+++ iconlib.lua
@@ -0,0 +1,31 @@
+-- // Planet's Software // 
+local icon = {
+  FluentI = { --FluentIcons
+		Home = "rbxassetid://9792462652",
+		Aimbot = "rbxassetid://9766671041",
+		Visuals = "rbxassetid://9766673555",
+		Player = "rbxassetid://9766672602",
+		Misc = "rbxassetid://9766671943",
+		Settings = "rbxassetid://9766674082",
+		Credits = "rbxassetid://9766675093",
+		Exit = "rbxassetid://9766676906"
+	},
+	FeatherI = { --FeatherIcons
+		Home = "rbxassetid://9792650361",
+		Aimbot = "rbxassetid://9792632523",
+		Visuals = "rbxassetid://9792631281",
+		Player = "rbxassetid://9792631906",
+		Misc = "rbxassetid://9792634811",
+		Settings = "rbxassetid://9792633222",
+		Credits = "rbxassetid://9792634075",
+		Exit = "rbxassetid://9792635572"
+	},
+	NotifiI = { --NotificationIcons
+		Success = "rbxassetid://9838874163",
+		Warning = "rbxassetid://9838873385",
+		Error = "rbxassetid://9838876113",
+		Informational = "rbxassetid://9838877673",
+		Custom = "rbxassetid://9838878267"
+	},
+	MainLogo = "" -- On working
+}
